1ca762d7dbfbf4eca4c7cf635dd2ad1850c7f779
Janis Streib <janis@dogcraft.de>
2016-03-04 21:42:48 +0100

    UPD: initialize config

--- src/de/unikarlsruhe/nan/pos/Main.java
+++ src/de/unikarlsruhe/nan/pos/Main.java
@@ -1,6 +1,10 @@
 package de.unikarlsruhe.nan.pos;
 
-import com.googlecode.lanterna.gui2.*;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.SQLException;
+
+import com.googlecode.lanterna.gui2.MultiWindowTextGUI;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
@@ -8,36 +12,37 @@ import com.googlecode.lanterna.terminal.ExtendedTerminal;
 import com.googlecode.lanterna.terminal.MouseCaptureMode;
 import com.googlecode.lanterna.terminal.Terminal;
 
-import java.io.IOException;
-
 /**
  * @author Anton Schirg
  */
 public class Main {
-    public static void main(String[] args) throws IOException {
-        Terminal terminal = new DefaultTerminalFactory().createTerminal();
-        System.err.println(terminal.getClass().getName());
-        if (terminal instanceof ExtendedTerminal) {
-            ((ExtendedTerminal) terminal).setMouseCaptureMode(MouseCaptureMode.CLICK_RELEASE);
-        } else {
-            System.exit(99);
-//            throw new RuntimeException("Terminal does not support mouse input");
-        }
-        Screen screen = new TerminalScreen(terminal);
-        screen.startScreen();
-        MultiWindowTextGUI gui = new MultiWindowTextGUI(screen);
+	public static void main(String[] args) throws IOException, SQLException {
+		new NANPosConfiguration(new FileInputStream("conf/nanpos.properties"));
+		Terminal terminal = new DefaultTerminalFactory().createTerminal();
+		System.err.println(terminal.getClass().getName());
+		if (terminal instanceof ExtendedTerminal) {
+			((ExtendedTerminal) terminal)
+					.setMouseCaptureMode(MouseCaptureMode.CLICK_RELEASE);
+		} else {
+			System.exit(99);
+			// throw new
+			// RuntimeException("Terminal does not support mouse input");
+		}
+		Screen screen = new TerminalScreen(terminal);
+		screen.startScreen();
+		MultiWindowTextGUI gui = new MultiWindowTextGUI(screen);
 
-        MainWindow window = new MainWindow();
+		MainWindow window = new MainWindow();
 
-        gui.addWindow(window);
-        gui.setActiveWindow(window);
+		gui.addWindow(window);
+		gui.setActiveWindow(window);
 
-        gui.waitForWindowToClose(window);
+		gui.waitForWindowToClose(window);
 
-        if (terminal instanceof UnixGpmTerminal) {
-            ((UnixGpmTerminal) terminal).stopGpm();
-        }
+		if (terminal instanceof UnixGpmTerminal) {
+			((UnixGpmTerminal) terminal).stopGpm();
+		}
 
-//        screen.stopScreen();
-    }
+		// screen.stopScreen();
+	}
 }
