8e3fb13f3f10fdee03b4ae49cfca69194318d264
Janis Streib <janis@dogcraft.de>
2018-04-15 22:01:02 +0200

    FIX: trigger input on card input

--- src/de/unikarlsruhe/nan/pos/tui/LoginWindow.java
+++ src/de/unikarlsruhe/nan/pos/tui/LoginWindow.java
@@ -1,170 +1,194 @@
 package de.unikarlsruhe.nan.pos.tui;
 
-import com.googlecode.lanterna.TerminalPosition;
-import de.unikarlsruhe.nan.pos.CardReader;
-import de.unikarlsruhe.nan.pos.objects.User;
-
 import java.security.NoSuchAlgorithmException;
 import java.sql.SQLException;
+
+import com.googlecode.lanterna.TerminalPosition;
+
+import de.unikarlsruhe.nan.pos.CardReader;
+import de.unikarlsruhe.nan.pos.objects.User;
 
 /**
  * @author Anton Schirg
  */
 public class LoginWindow extends Window {
 
-    private final CardReader.CardReaderListener cardReaderListener;
-    private final LoginResultHandler resultHandler;
-    private Button cancelButton = null;
-    private boolean check_pin;
+	private final CardReader.CardReaderListener cardReaderListener;
+	private final LoginResultHandler resultHandler;
+	private Button cancelButton = null;
+	private boolean check_pin;
 
-    public LoginWindow(final LoginResultHandler resultHandler, boolean withAsciiArt, String action, boolean cancelable, boolean check_pin) {
-        this.check_pin = check_pin;
-        CenterLayout centerLayout = new CenterLayout();
-        setCentralComponent(centerLayout);
-        VerticalLayout verticalLayout = new VerticalLayout();
-        centerLayout.addChild(verticalLayout);
+	public LoginWindow(final LoginResultHandler resultHandler,
+			boolean withAsciiArt, String action, boolean cancelable,
+			boolean check_pin) {
+		this.check_pin = check_pin;
+		CenterLayout centerLayout = new CenterLayout();
+		setCentralComponent(centerLayout);
+		VerticalLayout verticalLayout = new VerticalLayout();
+		centerLayout.addChild(verticalLayout);
 
-        if (withAsciiArt) {
-            AsciiArt asciiArt = new AsciiArt();
-            verticalLayout.addChild(asciiArt);
-        }
+		if (withAsciiArt) {
+			AsciiArt asciiArt = new AsciiArt();
+			verticalLayout.addChild(asciiArt);
+		}
 
-        if (action != null) {
-            Label messageLabel = new Label("Scan card to " + action);
-            verticalLayout.addChild(messageLabel);
-            Label messageLabel2 = new Label("or");
-            verticalLayout.addChild(messageLabel2);
-            Label messageLabel3 = new Label("Touch to " + action + " using PIN");
-            verticalLayout.addChild(messageLabel3);
-        }
+		if (action != null) {
+			Label messageLabel = new Label("Scan card to " + action);
+			verticalLayout.addChild(messageLabel);
+			Label messageLabel2 = new Label("or");
+			verticalLayout.addChild(messageLabel2);
+			Label messageLabel3 = new Label("Touch to " + action + " using PIN");
+			verticalLayout.addChild(messageLabel3);
+		}
 
-        final CardReader cardReader = CardReader.getInstance();
-        cardReaderListener = new CardReader.CardReaderListener() {
-            @Override
-            public boolean onCardDetected(String cardnr, String uid) {
-                if (LoginWindow.this.isVisible()) {
-                    try {
-                        User userByCardnr = User.getUserByCardnr(cardnr);
-                        if (userByCardnr != null) {
-                            cardReader.successAnimation();
-                            cardReader.disableListener();
-                            resultHandler.handle(userByCardnr, LoginWindow.this, "Success");
-                            return true;
-                        } else {
-                            cardReader.disableListener();
-                            resultHandler.handle(null, LoginWindow.this, "Unknown card");
-                            //cardReader.failAnimation();
-                        }
-                    } catch (SQLException e) {
-                        e.printStackTrace();
-                        resultHandler.handle(null, LoginWindow.this, "SQL Exception");
-                    } catch (NoSuchAlgorithmException e) {
-                        e.printStackTrace();
-                        resultHandler.handle(null, LoginWindow.this, "NoSuchAlgorithmException - WAT?");
-                    }
-                    return false;
-                } else {
-                    return false;
-                }
-            }
-        };
-        cardReader.setListener(cardReaderListener);
+		final CardReader cardReader = CardReader.getInstance();
+		cardReaderListener = new CardReader.CardReaderListener() {
+			@Override
+			public boolean onCardDetected(String cardnr, String uid) {
+				LoginWindow.this.getTui().fireGenericInputEvent();
+				if (LoginWindow.this.isVisible()) {
+					try {
+						User userByCardnr = User.getUserByCardnr(cardnr);
+						if (userByCardnr != null) {
+							cardReader.successAnimation();
+							cardReader.disableListener();
+							resultHandler.handle(userByCardnr,
+									LoginWindow.this, "Success");
+							return true;
+						} else {
+							cardReader.disableListener();
+							resultHandler.handle(null, LoginWindow.this,
+									"Unknown card");
+							// cardReader.failAnimation();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+						resultHandler.handle(null, LoginWindow.this,
+								"SQL Exception");
+					} catch (NoSuchAlgorithmException e) {
+						e.printStackTrace();
+						resultHandler.handle(null, LoginWindow.this,
+								"NoSuchAlgorithmException - WAT?");
+					}
+					return false;
+				} else {
+					return false;
+				}
+			}
+		};
+		cardReader.setListener(cardReaderListener);
 
-        if (cancelable) {
-            cancelButton = new Button("Cancel", new Runnable() {
-                @Override
-                public void run() {
-                    resultHandler.handle(null, LoginWindow.this, "Canceled");
-                }
-            });
-            verticalLayout.addChild(cancelButton);
-        }
-        this.resultHandler = resultHandler;
-    }
+		if (cancelable) {
+			cancelButton = new Button("Cancel", new Runnable() {
+				@Override
+				public void run() {
+					resultHandler.handle(null, LoginWindow.this, "Canceled");
+				}
+			});
+			verticalLayout.addChild(cancelButton);
+		}
+		this.resultHandler = resultHandler;
+	}
 
+	@Override
+	protected void onClick(TerminalPosition position) {
+		// Can click anywhere on screen but button
+		if (cancelButton != null
+				&& cancelButton.getPosition().isInside(position)) {
+			super.onClick(position);
+			return;
+		}
 
+		CardReader.getInstance().disableListener();
 
-    @Override
-    protected void onClick(TerminalPosition position) {
-        //Can click anywhere on screen but button
-        if (cancelButton != null && cancelButton.getPosition().isInside(position)) {
-            super.onClick(position);
-            return;
-        }
+		final Window kbdWindow = new Window();
+		CenterLayout centerLayout = new CenterLayout();
+		Keyboard userKbd = new Keyboard(new Keyboard.KeyboardResultHandler() {
+			@Override
+			public void handle(final String userText, Keyboard caller) {
+				kbdWindow.close();
+				if (userText != null) {
+					if (check_pin) {
+						final Window pinWindow = new Window();
+						CenterLayout centerLayout = new CenterLayout();
+						Numpad pinNpd = new Numpad(
+								new Numpad.NumpadResultHandler() {
+									@Override
+									public void handle(String pinText,
+											Numpad caller) {
+										pinWindow.close();
+										if (pinText != null) {
+											try {
+												User userByName = User.getUserByNameAndPin(
+														userText, pinText);
+												if (userByName != null) {
+													resultHandler.handle(
+															userByName,
+															LoginWindow.this,
+															"Success");
+												} else {
+													resultHandler.handle(null,
+															LoginWindow.this,
+															"Unknown user or wrong PIN");
+												}
+											} catch (SQLException e) {
+												e.printStackTrace();
+												resultHandler.handle(null,
+														LoginWindow.this,
+														"SQL Exception");
+											} catch (NoSuchAlgorithmException e) {
+												e.printStackTrace();
+												resultHandler
+														.handle(null,
+																LoginWindow.this,
+																"NoSuchAlgorithmException - WAT?");
+											}
+										}
+									}
+								}, true, "PIN");
+						centerLayout.addChild(pinNpd);
+						pinWindow.setCentralComponent(centerLayout);
+						getTui().openWindow(pinWindow);
+					} else {
+						try {
+							User userByName = User.getUserByName(userText);
+							if (userByName != null) {
+								resultHandler.handle(userByName,
+										LoginWindow.this, "Success");
+							} else {
+								resultHandler.handle(null, LoginWindow.this,
+										"Unknown user");
+							}
+						} catch (SQLException e) {
+							e.printStackTrace();
+							resultHandler.handle(null, LoginWindow.this,
+									"SQL Exception");
+						} catch (NoSuchAlgorithmException e) {
+							e.printStackTrace();
+							resultHandler.handle(null, LoginWindow.this,
+									"NoSuchAlgorithmException - WAT?");
+						}
+					}
+				}
+			}
+		}, "Username");
+		centerLayout.addChild(userKbd);
+		kbdWindow.setCentralComponent(centerLayout);
+		getTui().openWindow(kbdWindow);
+	}
 
-        CardReader.getInstance().disableListener();
+	@Override
+	public void close() {
+		CardReader.getInstance().disableListener();
+		super.close();
+	}
 
-        final Window kbdWindow = new Window();
-        CenterLayout centerLayout = new CenterLayout();
-        Keyboard userKbd = new Keyboard(new Keyboard.KeyboardResultHandler() {
-            @Override
-            public void handle(final String userText, Keyboard caller) {
-                kbdWindow.close();
-                if (userText != null) {
-                    if (check_pin) {
-                        final Window pinWindow = new Window();
-                        CenterLayout centerLayout = new CenterLayout();
-                        Numpad pinNpd = new Numpad(new Numpad.NumpadResultHandler() {
-                            @Override
-                            public void handle(String pinText, Numpad caller) {
-                                pinWindow.close();
-                                if (pinText != null) {
-                                    try {
-                                        User userByName = User.getUserByNameAndPin(userText, pinText);
-                                        if (userByName != null) {
-                                            resultHandler.handle(userByName, LoginWindow.this, "Success");
-                                        } else {
-                                            resultHandler.handle(null, LoginWindow.this, "Unknown user or wrong PIN");
-                                        }
-                                    } catch (SQLException e) {
-                                        e.printStackTrace();
-                                        resultHandler.handle(null, LoginWindow.this, "SQL Exception");
-                                    } catch (NoSuchAlgorithmException e) {
-                                        e.printStackTrace();
-                                        resultHandler.handle(null, LoginWindow.this, "NoSuchAlgorithmException - WAT?");
-                                    }
-                                }
-                            }
-                        }, true, "PIN");
-                        centerLayout.addChild(pinNpd);
-                        pinWindow.setCentralComponent(centerLayout);
-                        getTui().openWindow(pinWindow);
-                    } else {
-                        try {
-                            User userByName = User.getUserByName(userText);
-                            if (userByName != null) {
-                                resultHandler.handle(userByName, LoginWindow.this, "Success");
-                            } else {
-                                resultHandler.handle(null, LoginWindow.this, "Unknown user");
-                            }
-                        } catch (SQLException e) {
-                            e.printStackTrace();
-                            resultHandler.handle(null, LoginWindow.this, "SQL Exception");
-                        } catch (NoSuchAlgorithmException e) {
-                            e.printStackTrace();
-                            resultHandler.handle(null, LoginWindow.this, "NoSuchAlgorithmException - WAT?");
-                        }
-                    }
-                }
-            }
-        }, "Username");
-        centerLayout.addChild(userKbd);
-        kbdWindow.setCentralComponent(centerLayout);
-        getTui().openWindow(kbdWindow);
-    }
+	public interface LoginResultHandler {
+		public void handle(User user, LoginWindow caller, String detailMessage);
+	}
 
-    @Override
-    public void close() {
-        CardReader.getInstance().disableListener();
-        super.close();
-    }
-
-    public interface LoginResultHandler {
-        public void handle(User user, LoginWindow caller, String detailMessage);
-    }
-
-    @Override
-    public void onVisible() {
-        CardReader.getInstance().setListener(cardReaderListener);
-    }
+	@Override
+	public void onVisible() {
+		CardReader.getInstance().setListener(cardReaderListener);
+	}
 }
--- src/de/unikarlsruhe/nan/pos/tui/TUI.java
+++ src/de/unikarlsruhe/nan/pos/tui/TUI.java
@@ -7,100 +7,105 @@ import com.googlecode.lanterna.TerminalRectangle;
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.terminal.ClickListener;
+
 import de.unikarlsruhe.nan.pos.CardReader;
 
 public class TUI extends Component implements ClickListener {
-    private Screen screen;
+	private Screen screen;
 
-    private long lastInputTime = System.currentTimeMillis();
+	private long lastInputTime = System.currentTimeMillis();
 
-    private LinkedList<Window> windowStack = new LinkedList<>();
+	private LinkedList<Window> windowStack = new LinkedList<>();
 
-    public TUI(Screen screen) {
-        this.screen = screen;
-    }
+	public TUI(Screen screen) {
+		this.screen = screen;
+	}
 
-    public void openWindow(Window child) {
-        child.setParent(this);
-        child.setTui(this);
-        windowStack.add(child);
-        child.onVisible();
-        layout();
-        redraw();
-    }
+	public void openWindow(Window child) {
+		child.setParent(this);
+		child.setTui(this);
+		windowStack.add(child);
+		child.onVisible();
+		layout();
+		redraw();
+	}
 
-    public void closeWindow(Window window) {
-        CardReader.getInstance().disableListener();
-        int i = windowStack.indexOf(window);
-        if (i != -1) {
-            if (i == 0) {
-                System.exit(0); // TODO
-            } else {
-                while (windowStack.size() > i) {
-                    windowStack.removeLast();
-                }
-            }
-        }
-        windowStack.getLast().onVisible();
-        layout();
-        redraw();
-    }
+	public void closeWindow(Window window) {
+		CardReader.getInstance().disableListener();
+		int i = windowStack.indexOf(window);
+		if (i != -1) {
+			if (i == 0) {
+				System.exit(0); // TODO
+			} else {
+				while (windowStack.size() > i) {
+					windowStack.removeLast();
+				}
+			}
+		}
+		windowStack.getLast().onVisible();
+		layout();
+		redraw();
+	}
 
-    public void closeWindowsAbove(Window window) {
-        CardReader.getInstance().disableListener();
-        int i = windowStack.indexOf(window);
-        if (i != -1) {
-            while (windowStack.size() > i + 1) {
-                windowStack.removeLast();
-            }
-        }
-        windowStack.getLast().onVisible();
-        layout();
-        redraw();
-    }
+	public void closeWindowsAbove(Window window) {
+		CardReader.getInstance().disableListener();
+		int i = windowStack.indexOf(window);
+		if (i != -1) {
+			while (windowStack.size() > i + 1) {
+				windowStack.removeLast();
+			}
+		}
+		windowStack.getLast().onVisible();
+		layout();
+		redraw();
+	}
 
-    public Window getTopWindow() {
-        return windowStack.getLast();
-    }
+	public Window getTopWindow() {
+		return windowStack.getLast();
+	}
 
-    public void layout() {
-        layout(new TerminalRectangle(TerminalPosition.TOP_LEFT_CORNER,
-                screen.getTerminalSize()));
-    }
+	public void layout() {
+		layout(new TerminalRectangle(TerminalPosition.TOP_LEFT_CORNER,
+				screen.getTerminalSize()));
+	}
 
-    @Override
-    protected void layout(TerminalRectangle position) {
-        windowStack.getLast().layout(position);
-    }
+	@Override
+	protected void layout(TerminalRectangle position) {
+		windowStack.getLast().layout(position);
+	}
 
-    @Override
-    public void redraw() {
-        windowStack.getLast().redraw();
-        this.lastInputTime = System.currentTimeMillis();
-    }
+	@Override
+	public void redraw() {
+		windowStack.getLast().redraw();
+		this.lastInputTime = System.currentTimeMillis();
+	}
 
-    @Override
-    TerminalSize getPreferredSize() {
-        return screen.getTerminalSize();
-    }
+	@Override
+	TerminalSize getPreferredSize() {
+		return screen.getTerminalSize();
+	}
 
-    @Override
-    protected Screen getScreen() {
-        return screen;
-    }
+	@Override
+	protected Screen getScreen() {
+		return screen;
+	}
 
-    @Override
-    protected void onClick(TerminalPosition position) {
-        windowStack.getLast().onClick(position);
-        this.lastInputTime = System.currentTimeMillis();
-    }
+	@Override
+	protected void onClick(TerminalPosition position) {
+		windowStack.getLast().onClick(position);
+		fireGenericInputEvent();
+	}
 
-    public long getLastInputTime() {
-        return lastInputTime;
-    }
+	public void fireGenericInputEvent() {
+		this.lastInputTime = System.currentTimeMillis();
+	}
 
-    @Override
-    public void clicked(TerminalPosition position) {
-        onClick(position);
-    }
+	public long getLastInputTime() {
+		return lastInputTime;
+	}
+
+	@Override
+	public void clicked(TerminalPosition position) {
+		onClick(position);
+	}
 }
