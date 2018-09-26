e394c61add3b87419a029b6adad8d4f8b269edbb
Janis Streib <janis@dogcraft.de>
2018-09-27 00:12:39 +0200

    FMT

--- src/de/unikarlsruhe/nan/pos/CardReader.java
+++ src/de/unikarlsruhe/nan/pos/CardReader.java
@@ -1,120 +1,125 @@
 package de.unikarlsruhe.nan.pos;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.io.*;
-import java.util.LinkedList;
 
 /**
  * @author Anton Schirg
  */
 public class CardReader {
-    private static CardReader instance = null;
+	private static CardReader instance = null;
 
-    private Writer scanner_writer = null;
+	private Writer scanner_writer = null;
 
-    public static CardReader getInstance() {
-        if (instance == null) {
-            instance = new CardReader();
-        }
-        return instance;
-    }
+	public static CardReader getInstance() {
+		if (instance == null) {
+			instance = new CardReader();
+		}
+		return instance;
+	}
 
+	private CardReaderListener listener = null;
 
-    private CardReaderListener listener = null;
-    public CardReader() {
-        try {
-            scanner_writer = new FileWriter("/dev/ttyUSB0");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try(BufferedReader br = new BufferedReader(new FileReader("/dev/ttyUSB0"))) {
-                    String line;
-                    while((line = br.readLine()) != null) {
-                        handleLine(line);
-                    }
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-        thread.start();
-    }
+	public CardReader() {
+		try {
+			scanner_writer = new FileWriter("/dev/ttyUSB0");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		Thread thread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try (BufferedReader br = new BufferedReader(new FileReader(
+						"/dev/ttyUSB0"))) {
+					String line;
+					while ((line = br.readLine()) != null) {
+						handleLine(line);
+					}
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		thread.start();
+	}
 
-    private void handleLine(String line) {
-        String cardnr, uid;
-        try {
-            JSONObject jsonObject = new JSONObject(line);
-            JSONObject check_result = jsonObject.getJSONObject("check_result");
-            cardnr = check_result.getString("cardnr");
-            uid = check_result.getString("uid");
-        } catch (JSONException ex) {
-            ex.printStackTrace();
-            System.err.println("Received: " + line);
-            return;
-        }
-        callListeners(cardnr, uid);
-    }
+	private void handleLine(String line) {
+		String cardnr, uid;
+		try {
+			JSONObject jsonObject = new JSONObject(line);
+			JSONObject check_result = jsonObject.getJSONObject("check_result");
+			cardnr = check_result.getString("cardnr");
+			uid = check_result.getString("uid");
+		} catch (JSONException ex) {
+			ex.printStackTrace();
+			System.err.println("Received: " + line);
+			return;
+		}
+		callListeners(cardnr, uid);
+	}
 
-    private void callListeners(String cardnr, String uid) {
-        if (listener != null) {
-            if (listener.onCardDetected(cardnr, uid)) {
-                disableListener();
-            }
-        }
-    }
+	private void callListeners(String cardnr, String uid) {
+		if (listener != null) {
+			if (listener.onCardDetected(cardnr, uid)) {
+				disableListener();
+			}
+		}
+	}
 
-    private void sendState(char ch) {
-        try {
-            if (scanner_writer != null) {
-                scanner_writer.write(ch);
-                scanner_writer.flush();
-                System.err.println("Sent " + ch + " to card reader");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+	private void sendState(char ch) {
+		try {
+			if (scanner_writer != null) {
+				scanner_writer.write(ch);
+				scanner_writer.flush();
+				System.err.println("Sent " + ch + " to card reader");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-    public void successAnimation() {
-        sendState('s');
-    }
+	public void successAnimation() {
+		sendState('s');
+	}
 
-    public void failAnimation() {
-        sendState('f');
-    }
+	public void failAnimation() {
+		sendState('f');
+	}
 
-    public void disableListener() {
-        listener = null;
-        sendState('N');
-    }
+	public void disableListener() {
+		listener = null;
+		sendState('N');
+	}
 
-    public void setListener(CardReaderListener listener) {
-        this.listener = listener;
-        sendState('L');
-    }
+	public void setListener(CardReaderListener listener) {
+		this.listener = listener;
+		sendState('L');
+	}
 
-    public interface CardReaderListener {
-        /**
-         * @return valid scan - if true listener will be unregistered
-         */
-        public boolean onCardDetected(String cardnr, String uid);
-    }
+	public interface CardReaderListener {
+		/**
+		 * @return valid scan - if true listener will be unregistered
+		 */
+		public boolean onCardDetected(String cardnr, String uid);
+	}
 
-    public static void main(String[] args) {
-        CardReader cardReader = new CardReader();
-        cardReader.setListener(new CardReaderListener() {
-            @Override
-            public boolean onCardDetected(String cardnr, String uid) {
-                System.out.println("Card nr:" + cardnr + " uid:" + uid);
-                return true;
-            }
-        });
-    }
+	public static void main(String[] args) {
+		CardReader cardReader = new CardReader();
+		cardReader.setListener(new CardReaderListener() {
+			@Override
+			public boolean onCardDetected(String cardnr, String uid) {
+				System.out.println("Card nr:" + cardnr + " uid:" + uid);
+				return true;
+			}
+		});
+	}
 }
--- src/de/unikarlsruhe/nan/pos/tui/BuyWindow.java
+++ src/de/unikarlsruhe/nan/pos/tui/BuyWindow.java
@@ -18,249 +18,240 @@ import de.unikarlsruhe.nan.pos.objects.Utils;
  * @author Anton Schirg
  */
 public class BuyWindow extends Window {
-	private User user;
-	private final VerticalLayout verticalLayout;
-	private final Label balanceLabel;
-	private BuyWindowResultHandler resultCallback;
-	private final CenterLayout centerLayout;
-	private static final long TIMEOUT = 30 * 1000;
+    private User user;
+    private final VerticalLayout verticalLayout;
+    private final Label balanceLabel;
+    private BuyWindowResultHandler resultCallback;
+    private final CenterLayout centerLayout;
+    private static final long TIMEOUT = 30 * 1000;
 
-	public BuyWindow(final User user) {
-		this.user = user;
-		centerLayout = new CenterLayout();
-		setCentralComponent(centerLayout);
-		verticalLayout = new VerticalLayout();
-		centerLayout.addChild(verticalLayout);
+    public BuyWindow(final User user) {
+        this.user = user;
+        centerLayout = new CenterLayout();
+        setCentralComponent(centerLayout);
+        verticalLayout = new VerticalLayout();
+        centerLayout.addChild(verticalLayout);
 
         verticalLayout.addChild(new Label("Logged in as"));
-		balanceLabel = new Label("User: " + user.getName() + " | "
-				+ "Balance: " + Utils.formatPrice(user.getBalance()));
-		verticalLayout.addChild(balanceLabel);
-		GridLayout gridLayout = new GridLayout(4);
-		verticalLayout.addChild(gridLayout);
+        balanceLabel = new Label("User: " + user.getName() + " | "
+                + "Balance: " + Utils.formatPrice(user.getBalance()));
+        verticalLayout.addChild(balanceLabel);
+        GridLayout gridLayout = new GridLayout(4);
+        verticalLayout.addChild(gridLayout);
 
-		try {
-			List<Product> allProducts = Product.getAllProducts();
-			for (final Product product : allProducts) {
-				gridLayout.addChild(new Button(product.getName() + "\n"
-						+ Utils.formatPrice(product.getPrice()),
-						new Runnable() {
-							@Override
-							public void run() {
-								clickedProduct(product);
-							}
-						}));
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+        try {
+            List<Product> allProducts = Product.getAllProducts();
+            for (final Product product : allProducts) {
+                gridLayout.addChild(new Button(product.getName() + "\n"
+                        + Utils.formatPrice(product.getPrice()),
+                        new Runnable() {
+                            @Override
+                            public void run() {
+                                clickedProduct(product);
+                            }
+                        }));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
-		HorizontalLayout horizontalLayout = new HorizontalLayout();
-		verticalLayout.addChild(horizontalLayout);
+        HorizontalLayout horizontalLayout = new HorizontalLayout();
+        verticalLayout.addChild(horizontalLayout);
 
-		horizontalLayout.addChild(new Button("Back", new Runnable() {
-			@Override
-			public void run() {
+        horizontalLayout.addChild(new Button("Back", new Runnable() {
+            @Override
+            public void run() {
                 PS2BarcodeScanner.getInstance().removeBarcodeListener();
-				resultCallback.handle(null, true);
-			}
-		}));
+                resultCallback.handle(null, true);
+            }
+        }));
 
-		if (user.isOpeartor()) {
-			horizontalLayout.addChild(new Button("Add User", new Runnable() {
-				@Override
-				public void run() {
-					getTui().openWindow(
-							new CreateUserWindow(
-									new CreateUserWindow.CreateUserResultHandler() {
-										@Override
-										public void handle(boolean success,
-												final CreateUserWindow caller,
-												String detailMessage) {
-											final ResultScreen resultScreen = new ResultScreen(
-													detailMessage, success);
-											resultScreen
-													.setDoneCallback(new Runnable() {
-														@Override
-														public void run() {
-															caller.close();
-														}
-													});
-											getTui().openWindow(resultScreen);
-										}
-									}, user));
-				}
-			}));
+        if (user.isOpeartor()) {
+            horizontalLayout.addChild(new Button("Add User", new Runnable() {
+                @Override
+                public void run() {
+                    getTui().openWindow(
+                            new CreateUserWindow(
+                                    new CreateUserWindow.CreateUserResultHandler() {
+                                        @Override
+                                        public void handle(boolean success,
+                                                           final CreateUserWindow caller,
+                                                           String detailMessage) {
+                                            final ResultScreen resultScreen = new ResultScreen(
+                                                    detailMessage, success);
+                                            resultScreen
+                                                    .setDoneCallback(new Runnable() {
+                                                        @Override
+                                                        public void run() {
+                                                            caller.close();
+                                                        }
+                                                    });
+                                            getTui().openWindow(resultScreen);
+                                        }
+                                    }, user));
+                }
+            }));
 
-			horizontalLayout.addChild(new Button("All Users", new Runnable() {
-				@Override
-				public void run() {
-					getTui().openWindow(new UserOverview());
-				}
-			}));
-			horizontalLayout.addChild(new Button("Add Product", new Runnable() {
-				@Override
-				public void run() {
-					getTui().openWindow(
-							new CreateProductWindow(
-									new CreateProductWindow.CreateProductResultHandler() {
-										@Override
-										public void handle(
-												boolean success,
-												final CreateProductWindow caller,
-												String detailMessage) {
-											final ResultScreen resultScreen = new ResultScreen(
-													detailMessage, success);
-											resultScreen
-													.setDoneCallback(new Runnable() {
-														@Override
-														public void run() {
-															caller.close();
-														}
-													});
-											getTui().openWindow(resultScreen);
-										}
-									}, user));
-				}
-			}));
+            horizontalLayout.addChild(new Button("All Users", new Runnable() {
+                @Override
+                public void run() {
+                    getTui().openWindow(new UserOverview());
+                }
+            }));
+            horizontalLayout.addChild(new Button("Add Product", new Runnable() {
+                @Override
+                public void run() {
+                    getTui().openWindow(
+                            new CreateProductWindow(
+                                    new CreateProductWindow.CreateProductResultHandler() {
+                                        @Override
+                                        public void handle(
+                                                boolean success,
+                                                final CreateProductWindow caller,
+                                                String detailMessage) {
+                                            final ResultScreen resultScreen = new ResultScreen(
+                                                    detailMessage, success);
+                                            resultScreen
+                                                    .setDoneCallback(new Runnable() {
+                                                        @Override
+                                                        public void run() {
+                                                            caller.close();
+                                                        }
+                                                    });
+                                            getTui().openWindow(resultScreen);
+                                        }
+                                    }, user));
+                }
+            }));
 
-		}
+        }
 
-		PS2BarcodeScanner.getInstance().setBarcodeListener(
-				new PS2BarcodeScanner.BarcodeListener() {
-					@Override
-					public void barcodeScanned(long barCode) {
-						System.err.println("Scanned " + barCode);
-						try {
-							Product byEAN = Product.getByEAN(barCode);
-							if (byEAN != null) {
-								clickedProduct(byEAN);
-							} else {
-								resultCallback.handle("Unknown product", false);
-							}
-						} catch (SQLException e) {
-							e.printStackTrace();
-						}
-					}
-				});
-		try {
-			NoteValidator.getInstance().setListener(new NoteValidator.NoteListener() {
-				@Override
-				public boolean onNoteRead(int channel, int valueInEurCt) {
-					getTui().fireGenericInputEvent();
-					System.err.println("Read note");
-					return true;
-				}
+        PS2BarcodeScanner.getInstance().setBarcodeListener(
+                new PS2BarcodeScanner.BarcodeListener() {
+                    @Override
+                    public void barcodeScanned(long barCode) {
+                        System.err.println("Scanned " + barCode);
+                        try {
+                            Product byEAN = Product.getByEAN(barCode);
+                            if (byEAN != null) {
+                                clickedProduct(byEAN);
+                            } else {
+                                resultCallback.handle("Unknown product", false);
+                            }
+                        } catch (SQLException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+        NoteValidator.getInstance().setListener(new NoteValidator.NoteListener() {
+            @Override
+            public boolean onNoteRead(int channel, int valueInEurCt) {
+                getTui().fireGenericInputEvent();
+                System.err.println("Read note");
+                return true;
+            }
 
-				@Override
-				public void onNoteCredited(int channel, int valueInEurCt) {
-					try {
-						user.recharge(valueInEurCt);
-						ResultScreen res = new ResultScreen("Revalued " + valueInEurCt/100 +"€!", true);
-						getTui().openWindow(res);
-					} catch (SQLException e) {
-						ResultScreen res = new ResultScreen("SQL expetion!", false);
-						getTui().openWindow(res);
-						e.printStackTrace();
-					}
-				}
+            @Override
+            public void onNoteCredited(int channel, int valueInEurCt) {
+                try {
+                    user.recharge(valueInEurCt);
+                    ResultScreen res = new ResultScreen("Revalued " + valueInEurCt / 100 + "€!", true);
+                    getTui().openWindow(res);
+                } catch (SQLException e) {
+                    ResultScreen res = new ResultScreen("SQL expetion!", false);
+                    getTui().openWindow(res);
+                    e.printStackTrace();
+                }
+            }
 
-				@Override
-				public void onNoteRejected() {
-				}
-			});
-		} catch (IOException e) {
-			e.printStackTrace();
-			// Todo: error window
-		}
-		new Thread(new Runnable() {
+            @Override
+            public void onNoteRejected() {
+            }
+        });
+        new Thread(new Runnable() {
 
-			@Override
-			public void run() {
-				while (true) {
-					try {
-						if (getTui() != null
-								&& System.currentTimeMillis()
-										- getTui().getLastInputTime() > TIMEOUT) {
-							PS2BarcodeScanner.getInstance()
-									.removeBarcodeListener();
-							close();
-							return;
-						}
-						Thread.sleep(400);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-				}
-			}
-		}).start();
-	}
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        if (getTui() != null
+                                && System.currentTimeMillis()
+                                - getTui().getLastInputTime() > TIMEOUT) {
+                            PS2BarcodeScanner.getInstance()
+                                    .removeBarcodeListener();
+                            close();
+                            return;
+                        }
+                        Thread.sleep(400);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+    }
 
-	private void clickedProduct(Product product) {
-		try {
-			NoteValidator.getInstance().disableListener();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		PS2BarcodeScanner.getInstance().removeBarcodeListener();
-		try {
-			if (this.isVisible()) {
-				boolean success = false;
-				try {
-					success = user.buy(product);
-				} catch (SQLException e) {
-					e.printStackTrace();
-					resultCallback.handle(
-							"SQL Error - could not buy the product", false);
-					return;
-				}
-				String balance = "ERROR";
-				try {
-					user.reloadBalance();
-					balance = Utils.formatPrice(user.getBalance());
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
-				resultCallback
-						.handle(success ? "Big success - you have bought the product\nNew Balance: "
+    private void clickedProduct(Product product) {
+        NoteValidator.getInstance().disableListener();
+        PS2BarcodeScanner.getInstance().removeBarcodeListener();
+        try {
+            if (this.isVisible()) {
+                boolean success = false;
+                try {
+                    success = user.buy(product);
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                    resultCallback.handle(
+                            "SQL Error - could not buy the product", false);
+                    return;
+                }
+                String balance = "ERROR";
+                try {
+                    user.reloadBalance();
+                    balance = Utils.formatPrice(user.getBalance());
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+                resultCallback
+                        .handle(success ? "Big success - you have bought the product\nNew Balance: "
                                         + balance
                                         : "Fatal error - could not buy the product",
                                 success);
-			} else {
-				resultCallback
-						.handle("Fatal error - was not logged in. This should never happen.",
+            } else {
+                resultCallback
+                        .handle("Fatal error - was not logged in. This should never happen.",
                                 false);
-			}
-		} finally {
-			user = null;
-		}
-	}
+            }
+        } finally {
+            user = null;
+        }
+    }
 
-	public void setResultCallback(BuyWindowResultHandler resultCallback) {
-		this.resultCallback = resultCallback;
-	}
+    public void setResultCallback(BuyWindowResultHandler resultCallback) {
+        this.resultCallback = resultCallback;
+    }
 
-	@Override
-	TerminalSize getPreferredSize() {
-		return centerLayout.getPreferredSize();
-	}
+    @Override
+    TerminalSize getPreferredSize() {
+        return centerLayout.getPreferredSize();
+    }
 
-	@Override
-	protected void layout(TerminalRectangle position) {
-		centerLayout.layout(position);
-	}
+    @Override
+    protected void layout(TerminalRectangle position) {
+        centerLayout.layout(position);
+    }
 
-	@Override
-	void redraw() {
-		centerLayout.redraw();
-	}
+    @Override
+    void redraw() {
+        centerLayout.redraw();
+    }
 
-	@Override
-	protected void onClick(TerminalPosition position) {
-		centerLayout.onClick(position);
-	}
+    @Override
+    protected void onClick(TerminalPosition position) {
+        centerLayout.onClick(position);
+    }
 
-	public interface BuyWindowResultHandler {
-		public void handle(String result, boolean success);
-	}
+    public interface BuyWindowResultHandler {
+        public void handle(String result, boolean success);
+    }
 }
